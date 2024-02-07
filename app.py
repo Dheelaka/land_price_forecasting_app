f4ea2908ea574eddf050db39dff029d316d94651
Dheelaka Weerasinghe <144258593+Dheelaka@users.noreply.github.com>
2024-02-07 15:20:47 +0530

    Change App

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 import joblib
 from statsmodels.tsa.statespace.sarimax import SARIMAX
+import plotly.express as px
+from xgboost import XGBRegressor
 
 to_filter_df = pd.read_csv('14_for_dashboard.csv')
 columns_to_keep = ['main_city', 'price_land_pp', 'Hospital', 'National School', 'Railway Station', 'Bus Stop', 'Main Road']
@@ -15,7 +17,7 @@ filtered_df = to_filter_df[columns_to_keep].copy()
 X = filtered_df.drop('price_land_pp', axis=1)
 y = filtered_df['price_land_pp']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
 
 numerical_features = X.select_dtypes(include=['float64', 'int64']).columns
 categorical_features = ['main_city'] + list(X.select_dtypes(include=['object']).columns)
@@ -29,65 +31,106 @@ preprocessor = ColumnTransformer(
         ('cat', categorical_transformer, categorical_features)
     ])
 
-model = GradientBoostingRegressor()
+model = XGBRegressor(n_estimators=50, learning_rate=0.1)
 pipeline = Pipeline(steps=[('preprocessor', preprocessor),
                              ('model', model)])
 
 pipeline.fit(X_train, y_train)
 
+
 joblib.dump(pipeline, 'trained_model.joblib')
 
-loaded_model = joblib.load('trained_model.joblib')
 
-import streamlit as st
+@st.cache_resource
+def load_model():
+    return joblib.load("trained_model.joblib")
+
+@st.cache_data
+def load_cities():
+    f = pd.read_csv("City_list_File.csv")
+    return f["Cities"].to_list()
+
+loaded_model = load_model()
+
 import matplotlib.pyplot as plt
-import numpy as np
 
-st.title('Price Prediction Dashboard')
+st.title("Price Prediction Dashboard")
 
-default_main_city = ''
-default_numerical_values = {feature: float(X[feature].mean()) for feature in numerical_features}
+default_main_city = ""
+# default_numerical_values = {feature: float(X[feature].mean()) for feature in numerical_features}
 
-city_list = [''] + sorted(list(X['main_city'].unique())) 
-main_city_value = st.selectbox('Select main_city', city_list, key='main_city')
+city_list = load_cities()
+main_city_value = st.selectbox("Select main_city", city_list, key="main_city")
 if not main_city_value:
-    st.warning('Please Select the City')
+    st.warning("Please Select the City")
 
 # Store the selected city for later use
 selected_city = main_city_value
+Y = {}
 
-st.header('Input Features')
+st.header("Input Features")
 
-for feature in numerical_features:
-    if feature != 'main_city':
-        value = st.slider(f'Select Nearest Distance for a {feature}', float(X[feature].min()), float(X[feature].max()), 0.01)
-        X[feature] = value
+# for feature in numerical_features:
+#     if feature != 'main_city':
+#        value = st.slider(f'Select Nearest Distance for a {feature}', float(X[feature].min()), float(X[feature].max()), 0.01)
+#        Y[feature] = value
 
-if st.button('Predict'):
+Y["main_city"] = [selected_city]
+Y["Hospital"] = [
+    st.slider(f"Select Nearest Distance for a Hospital (km)", float(1), float(5), 0.01)
+]
+Y["National School"] = [
+    st.slider(f"Select Nearest Distance for a National School (km)", float(1), float(5), 0.01)
+]
+Y["Railway Station"] = [
+    st.slider(f"Select Nearest Distance for a Railway Station (km)", float(1), float(5), 0.01)
+]
+Y["Bus Stop"] = [
+    st.slider(f"Select Nearest Distance for a Bus Stop (km)", float(1), float(5), 0.01)
+]
+Y["Main Road"] = [
+    st.slider(f"Select Nearest Distance for a Main Road (Meters)", float(1), float(2500), 0.01)
+]
+
+df = pd.DataFrame(Y)
+
+if st.button("Predict"):
     if not selected_city:
-        st.warning('Please Select the City')
+        st.warning("Please Select the City")
     else:
         # Use the stored city value for prediction
-        X['main_city'] = selected_city
-        prediction = loaded_model.predict(X)
-        rounded_prediction = round(prediction[0] / 1000) * 1000  # Round to the nearest 1000
-        st.write(f'Ideal Value for your Land : {rounded_prediction}')
-        st.write(f'You can sell your Land in the range of : {rounded_prediction - rounded_prediction*0.5} to {rounded_prediction + rounded_prediction*0.5}')
+        prediction = loaded_model.predict(df)
+        rounded_prediction = (
+            round(prediction[0] / 1000) * 1000
+        )  # Round to the nearest 1000
+        st.write(f"Ideal Value for your Land : {rounded_prediction}")
+        st.write(
+            f"Market value for your land : {rounded_prediction - rounded_prediction*0.5} to {rounded_prediction + rounded_prediction*0.5}"
+        )
 
-time_series_df = pd.read_csv('13_for_modeling.csv')
+time_series_df = pd.read_csv("7_price_filtered.csv")
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+time_series_df['main_city'] = time_series_df['main_city'].str.lower()
+time_series_df['posted_date'] = pd.to_datetime(time_series_df['posted_date'])
 
-if st.button('See your land price in next 3 years'):
-    st.subheader('Land Price Forecast for the Next 3 Years')
+time_series_df['year'] = time_series_df['posted_date'].dt.year
+time_series_df['quarter'] = time_series_df['posted_date'].dt.quarter
 
-    # Use the stored city value for time series analysis
+st.set_option("deprecation.showPyplotGlobalUse", False)
+
+if st.button("See your land price in next 3 years"):
+    st.subheader("Land Price Forecast for the Next 3 Years")
+
+    train_data = time_series_df[time_series_df['posted_date'] < '2023-09-01']
+
+    prediction_period = pd.date_range(start='2024-01-01', end='2027-03-31', freq='Q')
+    prediction_data = pd.DataFrame({'posted_date': prediction_period, 'quarter': prediction_period.quarter})
+    
     user_city = selected_city
 
     city_df = time_series_df[time_series_df['main_city'] == user_city]
-    city_df['posted_date'] = pd.to_datetime(city_df['posted_date'])
-    city_df.set_index('posted_date', inplace=True)
 
+    city_df.set_index('posted_date', inplace=True)
     quarterly_avg = city_df['price_land_pp'].resample('Q').mean()
 
     train_size = int(len(quarterly_avg) * 0.8)
@@ -95,25 +138,38 @@ if st.button('See your land price in next 3 years'):
 
     order = (1, 1, 1)
     seasonal_order = (1, 1, 1, 8)
-    model = SARIMAX(train, order=order, seasonal_order=seasonal_order)
-    model_fit = model.fit(method='powell')
+    model = SARIMAX(train, order=order, seasonal_order=seasonal_order, enforce_stationarity=False)
+    model_fit = model.fit()
 
-    forecast_steps = 16  # Forecast for the next 3 years (12 quarters)
+    forecast_steps = len(test) + 12 
     forecast = model_fit.get_forecast(steps=forecast_steps)
 
-    forecast_index = pd.date_range(start=quarterly_avg.index[-1] + pd.DateOffset(months=3), periods=forecast_steps, freq='Q')
+    end_of_train_index = quarterly_avg.index[-1]
 
-    smoothed_forecast = forecast.predicted_mean.rolling(window=5, min_periods=1).mean()
+    forecast_index = pd.date_range(start='2024-01-01', periods=forecast_steps, freq='Q')
+
+    smoothed_forecast = forecast.predicted_mean.rolling(window=5, min_periods=1).mean() 
     smoothed_forecast = smoothed_forecast.shift(-2)
 
-    plt.figure(figsize=(12, 6))
-    plt.plot(forecast_index, smoothed_forecast, color='red', label='Forecasted Land Price per Perch', linestyle='--')
+    # Create a Plotly figure
+    fig = px.line(x=forecast_index, y=smoothed_forecast, labels={'x': 'Time', 'y': 'Average Land Price'},
+                title=f"Land Price Forecast for {user_city}")
 
-    plt.title(f"Land Price Forecast for {user_city}")
-    plt.xlabel('Time')
-    plt.ylabel('Average Land Price')
-    plt.legend()
-    plt.show()
-    st.pyplot()
+    fig.update_layout(
+        width=1400,
+        height=700,
+        title=dict(
+            text=f"Land Price Forecast for {user_city}",
+            x=0.5,  # Centered
+            y=0.95,  # Adjust as needed
+            xanchor='center',
+            #yanchor='top',
+            font=dict(size=23), automargin=True
+        )
+    )
+
+    # Update line properties
+    fig.update_traces(line=dict(color='red', dash='dash'))
+    st.plotly_chart(fig, use_container_width=True)
 
 
