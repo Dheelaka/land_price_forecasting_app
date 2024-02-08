c0f45e3d5a7ba2540d5bb78d7f3ab1b2f8a21892
Dheelaka Weerasinghe <144258593+Dheelaka@users.noreply.github.com>
2024-02-08 19:36:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ import joblib
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 import plotly.express as px
 from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 
-to_filter_df = pd.read_csv('14_for_dashboard.csv')
+to_filter_df = pd.read_csv('14_for_dashboard.csv', low_memory=False)
 columns_to_keep = ['main_city', 'price_land_pp', 'Hospital', 'National School', 'Railway Station', 'Bus Stop', 'Main Road']
 filtered_df = to_filter_df[columns_to_keep].copy()
 
@@ -31,7 +32,7 @@ preprocessor = ColumnTransformer(
         ('cat', categorical_transformer, categorical_features)
     ])
 
-model = XGBRegressor(n_estimators=50, learning_rate=0.1)
+model = RandomForestRegressor(n_estimators=50, max_depth= None)
 pipeline = Pipeline(steps=[('preprocessor', preprocessor),
                              ('model', model)])
 
@@ -39,7 +40,6 @@ pipeline.fit(X_train, y_train)
 
 
 joblib.dump(pipeline, 'trained_model.joblib')
-
 
 @st.cache_resource
 def load_model():
@@ -52,15 +52,25 @@ def load_cities():
 
 loaded_model = load_model()
 
-import matplotlib.pyplot as plt
+st.markdown("""
+    <h1 style='text-align: center;'>Forecasting Land Price in <br> Colombo District</h1>
+""", unsafe_allow_html=True)
 
-st.title("Price Prediction Dashboard")
+# st.title("Price Prediction Dashboard")
 
 default_main_city = ""
 # default_numerical_values = {feature: float(X[feature].mean()) for feature in numerical_features}
 
 city_list = load_cities()
-main_city_value = st.selectbox("Select main_city", city_list, key="main_city")
+# main_city_value = st.selectbox("Select main_city", city_list, key="main_city")
+
+st.markdown("<h5 style='text-align: left; margin-bottom: -500px;'>Please select your Main City</h5>", unsafe_allow_html=True)
+main_city_value = st.selectbox("", city_list, key="main_city")
+
+st.markdown("<h5 style='text-align: left; margin-bottom: -500px;'>Size of your land (Perch)</h5>", unsafe_allow_html=True)
+size_input = st.number_input("")
+land_size = float(size_input)
+
 if not main_city_value:
     st.warning("Please Select the City")
 
@@ -68,31 +78,59 @@ if not main_city_value:
 selected_city = main_city_value
 Y = {}
 
-st.header("Input Features")
+st.subheader("Insert the smallets distance to below places")
+
+st.markdown("""
+    <p style='font-size: 14px; font-style: italic;'>*If your distance is more than the maximum, please set it to max.</p>
+""", unsafe_allow_html=True)
 
 # for feature in numerical_features:
 #     if feature != 'main_city':
 #        value = st.slider(f'Select Nearest Distance for a {feature}', float(X[feature].min()), float(X[feature].max()), 0.01)
 #        Y[feature] = value
 
+# st.markdown("<h4 style='text-align: left;'>Select a value</h4>", unsafe_allow_html=True)
+
 Y["main_city"] = [selected_city]
-Y["Hospital"] = [
-    st.slider(f"Select Nearest Distance for a Hospital (km)", float(1), float(5), 0.01)
-]
-Y["National School"] = [
-    st.slider(f"Select Nearest Distance for a National School (km)", float(1), float(5), 0.01)
-]
-Y["Railway Station"] = [
-    st.slider(f"Select Nearest Distance for a Railway Station (km)", float(1), float(5), 0.01)
-]
-Y["Bus Stop"] = [
-    st.slider(f"Select Nearest Distance for a Bus Stop (km)", float(1), float(5), 0.01)
-]
-Y["Main Road"] = [
-    st.slider(f"Select Nearest Distance for a Main Road (Meters)", float(1), float(2500), 0.01)
-]
+
+
+st.markdown("<h5 style='text-align: left;'>For a Hospital (km)</h5>", unsafe_allow_html=True)
+Y["Hospital"] = [st.slider(f"Nearest Distance for a :orange[Hospital] (km)", float(1), float(5), 0.01, label_visibility="collapsed")]
+
+st.markdown("<h5 style='text-align: left;'>For a National School (km)</h5>", unsafe_allow_html=True)
+Y["National School"] = [st.slider(f"Nearest Distance for a :orange[National School] (km)", float(1), float(5), 0.01, label_visibility="collapsed")]
+
+st.markdown("<h5 style='text-align: left;'>For a Railway Station (km)</h5>", unsafe_allow_html=True)
+Y["Railway Station"] = [st.slider(f"Nearest Distance for a :orange[Railway Station] (km)", float(1), float(5), 0.01, label_visibility="collapsed")]
+
+st.markdown("<h5 style='text-align: left;'>For a Bus Stop (km)</h5>", unsafe_allow_html=True)
+Y["Bus Stop"] = [st.slider(f"Nearest Distance for a :orange[Bus Stop] (km)", float(1), float(5), 0.01, label_visibility="collapsed")]
+
+st.markdown("<h5 style='text-align: left;'>For a Main Road (Meters)</h5>", unsafe_allow_html=True)
+Y["Main Road"] = [st.slider(f"Nearest Distance for a :orange[Main Road] (Meters)", float(1), float(2500), 0.01, label_visibility="collapsed")]
 
 df = pd.DataFrame(Y)
+
+# Increase the size and center the button
+st.markdown(
+    """
+    <style>
+        /* Increase the size of the button */
+        .stButton>button {
+            height: 50px;
+            width: 700px;
+            font-size: 30px;
+        }
+        /* Center the button */
+        .stButton {
+            display: block;
+            margin: 10 auto;
+        }
+    </style>
+""",
+    unsafe_allow_html=True,
+)
+
 
 if st.button("Predict"):
     if not selected_city:
@@ -103,12 +141,25 @@ if st.button("Predict"):
         rounded_prediction = (
             round(prediction[0] / 1000) * 1000
         )  # Round to the nearest 1000
-        st.write(f"Ideal Value for your Land : {rounded_prediction}")
-        st.write(
-            f"Market value for your land : {rounded_prediction - rounded_prediction*0.5} to {rounded_prediction + rounded_prediction*0.5}"
-        )
 
-time_series_df = pd.read_csv("7_price_filtered.csv")
+        Total_value = rounded_prediction * land_size
+        st.markdown("<h5 style='text-align: left; margin-top: 30px;'>Current Total Value for your Land :</h5>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center;'>Rs. " + str(Total_value/1000000) + " Mn</h1>", unsafe_allow_html=True)
+
+        st.markdown("<h5 style='text-align: left;'>Ideal Value per Perch :</h5>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Rs. " + str(rounded_prediction/1000000) + " Mn</h2>", unsafe_allow_html=True)
+
+        st.markdown("<h5 style='text-align: left;'>Market value for your land :</h5>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Min = Rs. " + str((Total_value - Total_value*0.5)/1000000) + "Mn</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Max = Rs. " + str((Total_value + Total_value*0.5)/1000000) + "Mn</h2>", unsafe_allow_html=True)
+
+
+        # st.write(f"Ideal Value for your Land : {rounded_prediction}")
+        # st.write(f"Market value for your land : {rounded_prediction - rounded_prediction*0.5} to {rounded_prediction + rounded_prediction*0.5}")
+
+st.divider()
+
+time_series_df = pd.read_csv("7_price_filtered.csv", low_memory=False)
 
 time_series_df['main_city'] = time_series_df['main_city'].str.lower()
 time_series_df['posted_date'] = pd.to_datetime(time_series_df['posted_date'])
@@ -159,7 +210,7 @@ if st.button("See your land price in next 3 years"):
         width=1400,
         height=700,
         title=dict(
-            text=f"Land Price Forecast for {user_city}",
+            text=f"Per Perch Price Forecast for {user_city}",
             x=0.5,  # Centered
             y=0.95,  # Adjust as needed
             xanchor='center',
